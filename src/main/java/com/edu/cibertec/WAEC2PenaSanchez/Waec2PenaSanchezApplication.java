07f0ee580a13427e3062d5bda76d96d4b61e14b5
graldineps <geriifujioka@gmail.com>
2024-04-26 14:38:54 -0500

    Creacion del proyecto

---
+++ src/main/java/com/edu/cibertec/WAEC2PenaSanchez/Waec2PenaSanchezApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.WAEC2PenaSanchez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2PenaSanchezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2PenaSanchezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/cibertec/WAEC2PenaSanchez/Waec2PenaSanchezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.WAEC2PenaSanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2PenaSanchezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
